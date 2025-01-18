a57d5c4b418e7074009f4e8a060bc821c0bda9b9
Md. Aktaruzzaman <auzaman50.cse@gmail.com>
2025-01-18 20:45:30 +0600

    Update send-mail.php

--- send-mail.php
+++ send-mail.php
@@ -14,8 +14,8 @@ if ($_SERVER['REQUEST_METHOD']=="POST") {
         $mail -> isSMTP();
         $mail -> Host = "smtp.gmail.com";
         $mail -> SMTPAuth = true;
-        $mail -> Username = "g.smtp.cform@gmail.com";
-        $mail -> Password = "cbax dmap eqpn fghl";
+        $mail -> Username = "your_gmail_address";
+        $mail -> Password = "your_gmail_app_password";
         $mail -> SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail -> Port = 587;
 
